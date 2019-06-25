d7da0254aec80248647cf89c501653b9bf70f58e
Matt Mencel <matt@techminer.net>
2019-06-25 11:20:28 -0500

    descriptions

--- azure_spn/output.tf
+++ azure_spn/output.tf
@@ -1,7 +1,9 @@
 output "spn_object_id" {
-  value = "${azuread_service_principal.spn.id}"
+  description = "SPN Object ID"
+  value       = "${azuread_service_principal.spn.id}"
 }
 
 output "spn_application_id" {
-  value = "${azuread_application.spn.application_id}"
+  description = "SPN Application ID. Sometimes referred to the Client ID. Used for authenticating with the SPN."
+  value       = "${azuread_application.spn.application_id}"
 }
